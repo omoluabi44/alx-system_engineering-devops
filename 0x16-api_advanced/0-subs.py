d72ed08cfcd5ea7a6660247ca40b41a41e863fad
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-08-17 12:05:26 +0100

    task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+def number_of_subscribers(subreddit):
+    """
+    Return the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers.
+    """
+    # Construct the url
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+
+    # Make a request to the url
+    response = requests.get(url)
+    # Get the data from the response
+    data = response.json()
+
+    # Get the number of subscribers from the data
+    subscribers = data["data"]["subscribers"]
+
+    # Return the number of subscribers
+    return subscribers
