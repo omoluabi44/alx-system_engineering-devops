48d3ea9edc933e1be93f69b5be177e9a26f86cc9
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-08-17 12:34:25 +0100

    task 1 bugs fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,25 +3,21 @@
 
 import requests
 def number_of_subscribers(subreddit):
-    """
-    Return the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers.
-    """
-    # Construct the url
+    if subreddit is None or type(subreddit) is not str:
+        return 0
     url = "https://www.reddit.com/r/" + subreddit + "/about.json"
 
-    # Make a request to the url
-    response = requests.get(url)
-    # Get the data from the response
+    response = requests.get(url, timeout=10)
+
     data = response.json()
+    if "data" in data and "subscribers" in data["data"]:
+                sub =  data["data"]["subscribers"]
+                print(sub)
+                return sub
+    else:
+        return 0
+              
 
-    # Get the number of subscribers from the data
-    subscribers = data["data"]["subscribers"]
+    
 
-    # Return the number of subscribers
-    return subscribers
+ 
